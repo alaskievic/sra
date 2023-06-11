787999e4aed8ea4859565c05884a8a7837395905
Andrei Arminio Laskievic <alaskievic@gmail.com>
2023-06-11 12:23:18 -0300

    exploring folklore

---
+++ data/code/folklore.R
@@ -0,0 +1,15 @@
+# Load packages
+source("00_load_packages.R")
+
+
+
+############### 1. Folklore Dataset ##############
+
+folklore <- read_dta(here("data", "raw", "folklore", "Replication_Tables_Figures",
+                          "Berezkin_groups_Regressions_Ready.dta"))
+
+
+
+  
+  
+
