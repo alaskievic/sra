9e9f25171e36100c2cc584087f46fd77cb7d5fc7
Andrei Arminio Laskievic <alaskievic@gmail.com>
2023-06-20 01:58:33 -0300

    finishing crosswalk between murdock map and EA

--- data/code/02_murdock_map.R
+++ data/code/02_murdock_map.R
@@ -9,74 +9,70 @@ library(murdock)
 
 # Murdock's Map
 map_murdock <- murdock
-
 map_murdock <- st_as_sf(map_murdock)
 
-plot(map_murdock$geometry)
+# plot(map_murdock$geometry)
 
 # Extract the data
 map_data <- murdock@data
-
 map_data %>% dplyr::count(CodeType)
-
 
 # Load Lowes(2023) crosswalk
 ea_map_match <- read_csv(here("data", "raw", "murdock_map",
-                              "murdock_ea_join_indexmatch_toshare.csv")) %>%
-  dplyr::select(c("NAME", "EA_match", "exact_match", "noclustermatch", "ea_name")) %>%
+                              "murdock_ea_join_indexmatch_toshare_revised.csv")) %>%
+  dplyr::select(c("NAME", "EA_match", "exact_match", "noclustermatch", "ea_name", 
+                  "index_id", "LAT", "LON")) %>%
   mutate(match_name = case_when(exact_match == 0 ~ ea_name,
                                 exact_match == 1 ~ EA_match)) %>%
   mutate(match_name = str_to_lower(match_name))%>%
+  mutate(match_name = str_replace(match_name, " \\s*\\([^\\)]+\\)", "")) %>%
   rename("map_name" = NAME) %>%
-  dplyr::select(c("map_name", "ea_name", "match_name", "exact_match",
-                  "noclustermatch",))
+  dplyr::select(c("map_name", "ea_name", "match_name", "exact_match", "index_id", 
+                  "LAT", "LON"))
 
 # Load cleaned D-Place EA
 load(here("data", "output", "ea_dplace.Rda"))
 
 # Construct a concordance between EA-Dplace and Murdock Map
 ea_dplace %<>% filter(var_id == "EA005") %>%
-  dplyr::select(c("pref_name", "orig_name", "alt_name")) %>%
+  dplyr::select(c("pref_name", "orig_name", "soc_id")) %>%
   mutate(match_name = str_to_lower(orig_name)) %>%
   mutate(match_name = str_replace(match_name, " \\s*\\([^\\)]+\\)", ""))
 
 
 # Match with murdock map
-teste <- full_join(ea_dplace, ea_map_match, by = "match_name")
-
-# create measure based on Michalopoulos(2018)
-
-
-
+crosswalk_ea_map <- full_join(ea_map_match, ea_dplace, by = "match_name") %>%
+  filter(is.na(index_id) == FALSE)
+save(crosswalk_ea_map, file = here("data", "output", "crosswalk_ea_map.Rda"))
 
 
 ######################## 2 - Some Maps on EA Variables #########################
+rm(list=ls())
+
 # Murdock's Map
 map_murdock <- murdock
-
 map_murdock <- st_as_sf(map_murdock)
-
-plot(map_murdock$geometry)
 
 # Extract the data
 map_data <- murdock@data
-map_data %>% dplyr::count(CodeType)
 
-# Load Lowes(2023) crosswalk
-lowes_data <- read_csv(here("data", "raw", "murdock_map",
-                              "murdock_ea_join_indexmatch_toshare.csv")) %>%
-  dplyr::select(c("NAME", "EA_match", "exact_match", "noclustermatch",
-                  "ea_name", "v5")) %>%
-  rename("map_name" = NAME) %>%
-  mutate(map_name = str_to_lower(map_name))
+# Load revised Lowes(2023) crosswalk
+load(here("data", "output", "crosswalk_ea_map.Rda"))
 
 # Match to data to Murdock Map
 map_murdock %<>% rename("map_name" = NAME) %>%
-  mutate(map_name = str_to_lower(map_name)) %>%
   dplyr::select(c("map_name", "CodeType"))
 
-map_murdock <- full_join(map_murdock, lowes_data, by = "map_name") %>%
-  mutate(v5 = as.factor(v5))
+map_murdock <- inner_join(map_murdock, crosswalk_ea_map, by = "map_name")
+  
+## Join with D-Place data
+# Load cleaned D-Place EA
+load(here("data", "output", "ea_dplace.Rda"))
+
+ea_dplace %<>% filter(var_id == "EA005") %>%
+  mutate(code = as.factor(code))
+
+map_murdock <- left_join(map_murdock, ea_dplace, by = "soc_id")
 
 
 # Map of Africa
@@ -85,7 +81,7 @@ map_africa <- st_read(dsn = here("data", "raw", "spatial", "africa_shp",
 
 v5_map <- tm_shape(map_murdock) +
   tm_borders(col = "darkgrey", alpha = 0.5, lwd = 1.5) +
-  tm_fill(col = "v5", style = "cat", palette = "RdYlGn") + 
+  tm_fill(col = "code", style = "cat", palette = "RdYlGn") + 
   tm_shape(map_africa) + 
   tm_borders(col = "black", lwd = 2) +
   tm_layout(legend.position = c("left", "bottom"),
@@ -95,9 +91,9 @@ v5_map <- tm_shape(map_murdock) +
 v5_map
 
 teste <- st_drop_geometry(map_murdock)
-  
-tmap_save(murdock_africa, here("data", "output", "figures", 
-                               "murdock_africa.png"))
+
+tmap_save(v5_map, here("data", "output", "figures", 
+                               "murdock_map_v05.png"))
 
   
 # Using the original match from Revised Murdock Map
